0d8aba1d1972fffb0b46fb2eefb73fbc4a7fcd34
unknown <chepurnojv@gmail.com>
2022-06-18 14:23:12 +0300

    ngularFire2 Setup &amp; Client Service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAI7qW9HklKvKeNNnec_WhpPVpnLMSWx6k",
+    authDomain: "clientpanelprod-9dc4f.firebaseapp.com",
+    projectId: "clientpanelprod-9dc4f",
+    storageBucket: "clientpanelprod-9dc4f.appspot.com",
+    messagingSenderId: "257135654363",
+    appId: "1:257135654363:web:08f2e0916bd956fbea63fe",
+    measurementId: "G-TB05KPESSQ",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAI7qW9HklKvKeNNnec_WhpPVpnLMSWx6k",
+    authDomain: "clientpanelprod-9dc4f.firebaseapp.com",
+    projectId: "clientpanelprod-9dc4f",
+    storageBucket: "clientpanelprod-9dc4f.appspot.com",
+    messagingSenderId: "257135654363",
+    appId: "1:257135654363:web:08f2e0916bd956fbea63fe",
+    measurementId: "G-TB05KPESSQ",
+  },
 };
